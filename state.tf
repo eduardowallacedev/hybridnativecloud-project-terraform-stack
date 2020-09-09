700a326908413485445d545bf1a7fcd3a3e5ddf3
Eduardo Wallace <52458043+eduardowallacedev@users.noreply.github.com>
2020-09-08 21:44:48 -0300

    AJuste da variavel

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-12345"
+    bucket = "hackathon-fiap-scj35-335230"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
